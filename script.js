187f69a9b592e1884d4073a1a0bb059dcae3b43a
Daniel Zhang <danielz4734@gmail.com>
2024-06-24 00:47:20 -0700

    done

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-for (const i = 0; i < 20; i++) {
-    const newDiv = new Document.createElement("div");
-    newDiv.innerHTML = i;
-    console.log(i);
-}
+var timelineSwiper = new Swiper ('#timeline #swiper_container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '#swiper_pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper_slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '#swiper_button_next',
+  prevButton: '#swiper_button_prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
+});
