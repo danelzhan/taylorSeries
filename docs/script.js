ec8eb09bf4c86a401bc63e06284478adec982193
Daniel Zhang <danielz4734@gmail.com>
2024-06-25 01:14:11 -0700

    asdfasdf

--- docs/script.js
+++ docs/script.js
@@ -13,3 +13,33 @@ const observer = new IntersectionObserver((entries) => {
 
 hiddenElementsLeft.forEach((element) => observer.observe(element));
 hiddenElementsRight.forEach((element) => observer.observe(element));
+
+const fdiff = document.querySelector('#fdiff');
+const parts = document.querySelector('#parts');
+const series = document.querySelector('#series');
+
+var text = document.querySelector('#contribution_description')
+
+fdiff.addEventListener('mouseover', () => {
+    text.innerHTML = "Mathematics of Finite Differences";
+    console.log("hovered");
+});
+fdiff.addEventListener('mouseout', () => {
+    text.innerHTML = "";
+});
+
+parts.addEventListener('mouseover', () => {
+    text.innerHTML = "Differentiation by Parts";
+    console.log("hovered");
+});
+parts.addEventListener('mouseout', () => {
+    text.innerHTML = "";
+});
+
+series.addEventListener('mouseover', () => {
+    text.innerHTML = "Taylor Polynomials";
+    console.log("hovered");
+});
+series.addEventListener('mouseout', () => {
+    text.innerHTML = "";
+});
